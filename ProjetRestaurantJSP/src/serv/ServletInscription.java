0f4ed9b39dabbf975af049153c30be02929bec65
MehdiLahmar <mehdilahmar@outlook.com>
2022-06-30 20:01:28 +0200

    Ajout de l'inscription/connexion

--- ProjetRestaurantJSP/src/serv/ServletInscription.java
+++ ProjetRestaurantJSP/src/serv/ServletInscription.java
@@ -1,23 +1,28 @@
 package serv;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.DaoClientImpl;
+import model.Client;
+
 /**
- * Servlet implementation class Login
+ * Servlet implementation class ServletInscription
  */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
+@WebServlet("/ServletInscription")
+public class ServletInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public ServletInscription() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,6 +32,22 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Client c = new Client();
+			c.setId(Integer.parseInt(request.getParameter("identifiant")));
+			c.setMdp(request.getParameter("mdp"));
+			c.setNom(request.getParameter("nom"));
+			c.setPrenom(request.getParameter("prenom"));
+			c.setAdresse(request.getParameter("adresse"));
+			
+		try {
+			new DaoClientImpl().create(c);
+			request.getRequestDispatcher("WEB-INF/ConfirmationInscription.jsp").forward(request, response);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
